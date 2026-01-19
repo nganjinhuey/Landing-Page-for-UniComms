b2cf00d32e9b4004da3b73e84c1a659068f7c4bb
Ngan Jin Huey <JHngan@Jin-MacBook-Air.local>
2026-01-19 13:27:58 +0800

    Remove multi-language links and routes

--- App.tsx
+++ App.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import HomeBM from './pages/HomeBM';
-import HomeCN from './pages/HomeCN';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/bm" element={<HomeBM />} />
-        <Route path="/cn" element={<HomeCN />} />
       </Routes>
     </Router>
   );
